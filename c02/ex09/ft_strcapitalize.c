3bb9c1f3046cfea4481d38f10049b94e8b9365b4
Simon <simon.aeby@laendrun.ch>
2022-09-02 08:03:05 +0200

    not working but I see how I should do this

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char *ft_strcapitalize(char *str)
+{
+	if str[i-1] >= ' ' && str[i -1] <= '/'
+		str[i] = capitalize
+	if str[i-1] >= ':' && str[i-1] <= '@' 
+		str[i] = capitalize
+	if str[i-1] >= '[' && str[i-1] <= '`'
+		str[i] = capitalize
+	if str[i-1] >= '{' && str[i-1] <= '~' 
+}
+
+int main(void)
+{
+	char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+1";
+	char expected_output[] = "Salut, Comment tu vas ? 42mots Quarante-Deux; Cinquante+Et+Un";
+	printf("%s", ft_strcapitalize(str);
+}
