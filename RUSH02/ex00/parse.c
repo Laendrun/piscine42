dc070197908f61b7be9ad0e4ce24cdca2e8f93d9
Simon Aeby <saeby@c1r11s8.42lausanne.ch>
2022-09-21 17:54:54 +0200

    rush02

---
+++ RUSH02/ex00/parse.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saeby <marvin@42lausanne.ch>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/17 13:52:01 by saeby             #+#    #+#             */
+/*   Updated: 2022/09/18 21:51:53 by saeby            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush.h"
+
+char	*ft_strdup(char *src, int offset)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc((offset + 2) * sizeof(char));
+	i = 0;
+	while (i <= offset)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+t_number	*parse(char *file)
+{
+	//int			f_size;
+	char		*buffer;
+	t_number	*nums;
+	int			c[5];
+
+	c[3] = file_size(file);
+	c[4] = count_lines(file);
+	nums = alloc_nums_tab(count_lines(file) + 1);
+	buffer = alloc_buffer(file_size(file) + 2);
+	if (fill_buffer(buffer, file) == -10)
+		return (NULL);
+	c[0] = 0;
+	c[1] = 0;
+	while (c[0] < c[3])
+	{
+		parse_helper(c, buffer, nums);
+	}
+	if (nums[count_lines(file) - 1].int_num[0] == '\0')
+	{
+		dict_error();
+		return (NULL);
+	}
+	return (nums);
+}
